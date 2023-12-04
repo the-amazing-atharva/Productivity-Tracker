613220c718886ae32c95ac6ba4658b08077b9455
Atharva <learnsocialacc.atharva@gmail.com>
2023-12-05 01:13:24 +0530

    hero section added

--- script.js
+++ script.js
@@ -14,19 +14,51 @@ form.addEventListener("submit", (e) => {
   addTodo();
 });
 
-function addTodo(todo) {
-  let todoText = input.value;
+// function addTodo(todo) {
+//   let todoText = input.value;
 
-  if (todo) {
-    todoText = todo.text;
-  }
+//   if (todo) {
+//     todoText = todo.text;
+//   }
 
-  if (todoText) {
+//   if (todoText) {
+//     const todoEl = document.createElement("li");
+//     if (todo && todo.completed) {
+//       todoEl.classList.add("completed");
+//     }
+
+//     todoEl.innerText = todoText;
+
+//     todoEl.addEventListener("click", () => {
+//       todoEl.classList.toggle("completed");
+//       updateLS();
+//     });
+
+//     todoEl.addEventListener("contextmenu", (e) => {
+//       e.preventDefault();
+
+//       todoEl.remove();
+//       updateLS();
+//     });
+
+//     todosUL.appendChild(todoEl);
+
+//     input.value = "";
+
+//     updateLS();
+//   }
+// }
+
+function addTodo() {
+  let todoText = input.value.trim(); // Trim any extra whitespace
+
+  // Check if the input value is not empty and is not a link text from the navbar
+  const isNavbarLink = Array.from(
+    document.querySelectorAll(".main-nav-link")
+  ).some((link) => link.innerText === todoText);
+
+  if (todoText && !isNavbarLink) {
     const todoEl = document.createElement("li");
-    if (todo && todo.completed) {
-      todoEl.classList.add("completed");
-    }
-
     todoEl.innerText = todoText;
 
     todoEl.addEventListener("click", () => {
@@ -36,15 +68,12 @@ function addTodo(todo) {
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
-
       todoEl.remove();
       updateLS();
     });
 
     todosUL.appendChild(todoEl);
-
     input.value = "";
-
     updateLS();
   }
 }
